84d68b14bc78263aaef096e97f1ac74192c6780f
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-09-25 10:52:14 -0300

    Lets Code!

---
+++ LetsCode.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main() {
+
+    char a;
+
+    printf("Digite uma letra: "); 
+    scanf("%c", &a);
+
+    printf("Letra no codigo de ASCII: %d\n", a );
+}
--- teste.c
+++ teste.c
@@ -1,24 +1,16 @@
 #include <stdio.h> 
+#include <stdlib.h>
 
 int main() {
+
+    int n = 50; 
+    float pi = 3.14159;
+    int k = 2;
     
-    int vect[20] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20}; 
-    int i; 
-
-
-    printf("Out \n");
-    for (i = 0; i < 20; i++){
-
-        if(vect[i] % 3 == 0) {
-            continue;
-        }
-        
-        if (vect[i] == 15) {
-            break;
-        } 
-        printf("%d ", vect[i]);
-    }
+    printf("Saida: \n %.2f\n  %.1f\n  %.1f\n  %03d\n 0%d\n  %d%20d\n    ", pi, (pi * 2), (float) (n * pi), k, (n / 2), (n * k), n );
 
 
     return 0;
+
+
 }
