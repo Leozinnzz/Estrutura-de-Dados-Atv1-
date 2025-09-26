67a44ac5b9ab282252c7fa5652070bba0b7e0513
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-09-26 15:59:57 -0300

    Falta terminar essa atividade

--- AtidadeR.c
+++ AtidadeR.c
@@ -1,21 +1,19 @@
-#include <stdio.h> 
+#include <stdio.h>
 
 int main() {
 
-    int num1, num2, num3;
-    int troca;
+    int a;
+    int b;
 
-    printf("Digite 3 valores: \n");
-    scanf("%d", &num1); 
-    scanf("%d", &num2);
-    scanf("%d", &num3); 
-
-    
-    if (num1 > num2) { troca = num1; num1 = num2; num2 = troca;} 
-    if (num2 > num3) { troca = num2; num2 = num3; num3 = troca;}
-    if (num1 > num2) { troca = num1; num1 = num2; num2 = troca;} 
-
-    printf("Ordem crescente: %d %d %d\n", num1, num2, num3);
-    printf("Ordem decrescente: %d %d %d\n", num3,  num2, num1);
-    
+printf("Digite dois valores: \n"); 
+scanf("%d", &a);
+scanf("%d", &b);
+    while (a % 2 != b % 2) {
+        scanf("%d", &a);
+        scanf("%d", &b);
+        if (a % 2 == b % 2) {
+            printf("%d\n", a);
+            printf("%d\n", b);
+        }
+    }  
 }
