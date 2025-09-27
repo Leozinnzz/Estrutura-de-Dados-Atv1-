718caa6e6f53240b6a363e55767bb175bbfeab59
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-09-27 11:53:00 -0300

    Descobri para que serve o boolean

--- AtidadeR.c
+++ AtidadeR.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int main() { 
+int main () {
     
-    int i, m, j; 
-    char n;
+    int N, i; 
+    bool primo = true;
 
-    printf("Digite qualquer digito: ");
-    scanf("%c", &n);
-    for (j = 6; j >= 1; j--) {
-        m = j;
-        for (i = m; i >= 1; i--) {
-            printf("%c ", n);
-        }
-        printf("\n");
+
+    printf("Digite um numero: ");
+    scanf("%d", &N); 
+
+    if (N <= 1) {
+        primo = false; 
+    } else {
+        for (i = 2; i <= N / 2; i++){
+            if (N % i == 0) {
+                primo = false;
+                break;
+            }
+        } 
     }
 
+    if(primo) {
+        printf("%d e um numero primo\n", N);
+    } else {
+        printf("%d nao e um numero primo\n", N);
+    }
 
-
+    return 0;
 }
+
