de0b3ad66bbe6d79ada80108a8a00057d706f5d7
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-09-29 09:06:01 -0300

    quinta atividade do dia

--- LetsCode.c
+++ LetsCode.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    float media, n1, n2, n3;
-    int p1, p2, p3;
+    int n1, n2, n3, n4, n5; 
+    int maior, menor;
 
-    printf("Digite 3 notas: \n");
-    scanf("%f %f %f", &n1, &n2, &n3); 
+    printf("Digite 5 notas: \n"); 
+    scanf("%d %d %d %d %d", &n1, &n2, &n3, &n4, &n5);
 
-    if(n1 >= n2 && n1 >= n3) {
-        p1 = 4; p2 = 3; p3 = 3; 
-    } 
-    else if (n2 >= n1 && n2 >= n3) {
-        p1 = 3; p2 = 4; p3 = 3;
-    }
-    else {
-        p1 = 3; p2 = 3; p3 = 4;
-    }
+    int soma = n1 + n2 + n3 + n4 + n5;
+    maior = n1;
+    menor = n1; 
 
-    media = (n1 * p1 + n2 * p2 + n3 * p3) / (p1 + p2 + p3);
+    if (n2 > maior) maior = n2;
+    if (n3 > maior) maior = n3;
+    if (n4 > maior) maior = n4; 
+    if (n5 > maior) maior = n5; 
 
-    if (media >= 6) {
-        printf("Aprovado!\n");
-    }
-    else {
-        printf("Reprovado!\n");
-    }
+    if (n2 < menor) menor = n2; 
+    if (n3 < menor) menor = n3;
+    if (n4 < menor) menor = n4; 
+    if (n5 < menor) menor = n5;
 
-    printf("Media ponderada %.2f \n", media);
+    printf("Media das notas %.2f\n", (float) (soma - maior - menor) / 3);
+} 
 
-    
-
-}
