1881ed7446fcc8ae9623e918966cc5f0eb3d490c
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-10-01 11:22:17 -0300

    Inverted right half pyramid

--- LetsCode.c
+++ LetsCode.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 int main() {
 
-    int n, i, j; 
+    int n; 
 
-    printf("Digite um valor: ");
-    scanf("%d", &n); 
+    printf("quantas linhas: ");
+    scanf("%d", &n);
 
-    for (int i = 0; i <= n; i++) {
-        for (j = 1; j <=  n - i; j++) {
-            printf(" ");
-        }
-
-        for (j = 1; j <=  2 * i - 1; j++) {
-            printf("*");
+    for(int i=0; i < n; i++) {
+        for (int j = 0; j < n - i; j++) { 
+            printf(" * ");
         }
         printf("\n");
     }
+    return 0;
+
 }
