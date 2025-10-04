03d48e2c59fd9ba19878bc321fef2148ca373d2e
Leozera Magalhaes <leozerathelegend@gmail.com>
2025-10-04 17:57:49 -0300

    ReadMe e ajustes

--- atividade11.c
+++ atividade11.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-#include <limits.h>
+
 
 int main() {
 
     int n, maior, cont = 0, menor; 
 
-    
-    maior = INT_MIN;
-    menor = INT_MAX;
 
     printf("Digite um valor: ");
     scanf("%d", &n); 
@@ -18,7 +15,6 @@ int main() {
     for(int i=n; i >= 0; i--){
         if(i % 2 == 0) {
             printf("%d\n", i);
-            maior = (i > maior) ? i : maior;
         }
     }
 
@@ -27,10 +23,6 @@ int main() {
     for(int j = 0; j <= n; j++) {
         if(j % 2 != 0) {
             printf("%d\n", j);
-            menor = j < menor ? j : menor;
         }
     }
-
-    printf("Maior numero: %d\n", maior);
-    printf("Menor numero: %d\n", menor);
 }
